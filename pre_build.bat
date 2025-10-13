422d0f19af3a329d1ad1e4dacb0af2318f8548b8
Jakob <jakob.frenzel@hotmail.com>
2025-10-13 18:58:35 +0200

    updated pre build to add .cproject to ignored git files, removed deletion of old version_strings.h because its already removed by post build.

--- pre_build.bat
+++ pre_build.bat
@@ -1,9 +1,11 @@
 @echo off
 echo pre build process started
 
+REM ignore changes to project setup
+git update-index --assume-unchanged ..\.cproject
+REM git update-index --no-assume-unchanged .\.cproject
+
 echo Generating Git Version...
-REM remove old file
-del version_strings.h
 
 REM call script
 ..\inav_tc375\src\utils\aurix_generate_version_strings.sh
