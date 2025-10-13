1a957a5b29ed74d0ace6011aff656876a50bfbe9
Jakob <jakob.frenzel@hotmail.com>
2025-10-13 18:39:07 +0200

    updated generate settings to generate in release folder

--- generate_settings.bat
+++ generate_settings.bat
@@ -1,7 +1,7 @@
 @echo off
-cd "TriCore Debug (GCC)\"
+cd "TriCore Release (GCC)\"
 wsl ../inav_tc375/src/utils/aurix_generate_settings.sh
 
 REM copy files to other build config
-copy /Y settings_generated.h "..\TriCore Release (GCC)\"
-copy /Y settings_generated.c "..\TriCore Release (GCC)\"
+copy /Y settings_generated.h "..\TriCore Debug (GCC)\"
+copy /Y settings_generated.c "..\TriCore Debug (GCC)\"
