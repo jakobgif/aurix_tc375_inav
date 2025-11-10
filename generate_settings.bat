6cba531448892647b7922fd24ed293c4e26b7c19
Jakob <jakob.frenzel@hotmail.com>
2025-11-10 17:31:29 +0100

    only copy settings if the directory exists

--- generate_settings.bat
+++ generate_settings.bat
@@ -2,6 +2,9 @@
 cd "TriCore Release (GCC)\"
 wsl ../inav_tc375/src/utils/aurix_generate_settings.sh
 
+REM check if "..\TriCore Debug (GCC)\" exists
+if exist "..\TriCore Debug (GCC)\" (
 REM copy files to other build config
 copy /Y settings_generated.h "..\TriCore Debug (GCC)\"
 copy /Y settings_generated.c "..\TriCore Debug (GCC)\"
+)
