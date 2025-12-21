84d08156f463b0edd637a38e7d5defda3b4fcb3d
Jakob <jakob.frenzel@hotmail.com>
2025-12-21 11:28:16 +0100

    .cproject asssumed unchanged is disabled in the post build

--- post_build.bat
+++ post_build.bat
@@ -1,6 +1,9 @@
 @echo off
 echo post build process started
 
+REM changes to these files where ignored for building.
+git update-index --no-assume-unchanged .\.cproject
+
 REM remove version strings file
 del version_strings.h
 
