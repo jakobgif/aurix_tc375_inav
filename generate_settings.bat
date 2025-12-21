da2cbb53859de67a1b551e05a32a7b91a9808a07
Jakob <jakob.frenzel@hotmail.com>
2025-12-21 11:39:02 +0100

    create debug folder if it does not exist

--- generate_settings.bat
+++ generate_settings.bat
@@ -2,9 +2,11 @@
 cd "TriCore Release (GCC)\"
 wsl ../inav_tc375/src/utils/aurix_generate_settings.sh
 
-REM check if "..\TriCore Debug (GCC)\" exists
-if exist "..\TriCore Debug (GCC)\" (
+REM ensure "..\TriCore Debug (GCC)\" exists
+if not exist "..\TriCore Debug (GCC)\" (
+    mkdir "..\TriCore Debug (GCC)\"
+)
+
 REM copy files to other build config
 copy /Y settings_generated.h "..\TriCore Debug (GCC)\"
 copy /Y settings_generated.c "..\TriCore Debug (GCC)\"
-)
