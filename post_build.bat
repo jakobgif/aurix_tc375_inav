1a9adeea73692cbd14b145153a62b74aefb5ffcd
Jakob <jakob.frenzel@hotmail.com>
2026-01-05 09:26:48 +0100

    fixed cproject path in post build

--- post_build.bat
+++ post_build.bat
@@ -2,7 +2,7 @@
 echo post build process started
 
 REM changes to these files where ignored for building.
-git update-index --no-assume-unchanged .\.cproject
+git update-index --no-assume-unchanged ..\.cproject
 
 REM remove version strings file
 del version_strings.h
